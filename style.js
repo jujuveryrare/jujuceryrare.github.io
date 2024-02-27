813c4e112347d20148b4c9623f5b534d3028abf1
jujuveryrare <161485423+jujuveryrare@users.noreply.github.com>
2024-02-27 07:56:08 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,15 @@
+console.log("mando eats boody");
+
+
+const buttonone = document.getElementById("hed1");
+
+buttonone.addEventListener("click", function() {
+   alert("Strap in! Click that link for an express ticket to the official Supra galaxy. Get ready to zoom through the digital Autobahn straight to Supra greatness. It's a joyride of horsepower and hilarity your destination? Smiles per Hour!")
+});
+
+function changecolor() {
+    document.getElementById("hed1").style.color = "black"
+ }
+ 
+ 
+
